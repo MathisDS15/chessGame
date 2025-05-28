399a929969bc54d4c8b43f2aafe5aa1bb3de64f6
Mathis DA SILVA <mathisds1506@gmail.com>
2025-05-28 15:41:56 +0200

    Update Pawn.java

--- src/fr/eseo/e3e/devlogiciel/projet/piece/Pawn.java
+++ src/fr/eseo/e3e/devlogiciel/projet/piece/Pawn.java
@@ -10,35 +10,36 @@ public class Pawn {
         // Logique de déplacement du pion
         scanner = new Scanner(System.in);
         boolean isDeuxCases = false;
-        int positionX = 0;
-        int positionY = 0;
+        int positionPawnX = 0;
+        int positionPawnY = 0;
         System.out.println("Le pion est à la position : " + x + ", " + y);
-        while(positionX !=x){
+        while(positionPawnX !=x){
             System.out.println("Sur quelle case voulez-vous déplacer le pion ? la position X :");
-            positionX = scanner.nextInt();
+            positionPawnX = scanner.nextInt();
             scanner.nextLine();
-            if(positionX !=x){
+            if(positionPawnX !=x){
                 System.out.println("Le pion ne peut pas se déplacer de cette façon.");
             }
         }
         System.out.println("Sur quelle case voulez-vous déplacer le pion ? la position Y :");
-        positionY = scanner.nextInt();
+        positionPawnY = scanner.nextInt();
         scanner.nextLine();
 
-        if (y==6 && y-positionY==2 ) {
+        if (y==6 && y-positionPawnY==2 ) {
             isDeuxCases =true;
             System.out.println("Vous avez déplacé le pion de deux cases.");
-            System.out.println("Vous avez déplacé le pion à la position : " + positionX + ", " + positionY);
-        } else if (y-positionY==1 ) {
+            System.out.println("Vous avez déplacé le pion à la position : " + positionPawnX + ", " + positionPawnY);
+        } else if (y-positionPawnY==1 ) {
             System.out.println("Vous avez déplacé le pion d'une case.");
-            System.out.println("Vous avez déplacé le pion à la position : " + positionX + ", " + positionY);
+            System.out.println("Vous avez déplacé le pion à la position : " + positionPawnX + ", " + positionPawnY);
         } else {
             System.out.println("Déplacement invalide. Le pion ne peut se déplacer que d'une ou deux cases vers l'avant.");
             return;
         }
 
+    }
 
-
+    public void promotion(){
 
     }
 
