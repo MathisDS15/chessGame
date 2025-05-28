b289a9b6f4a6636764ab19db438d483996e69461
Mathis DA SILVA <mathisds1506@gmail.com>
2025-05-28 15:40:59 +0200

    Update King.java

--- src/fr/eseo/e3e/devlogiciel/projet/piece/King.java
+++ src/fr/eseo/e3e/devlogiciel/projet/piece/King.java
@@ -1,4 +1,32 @@
 package fr.eseo.e3e.devlogiciel.projet.piece;
 
+import java.util.Scanner;
+
 public class King {
+
+    Scanner scanner;
+
+    public void selection(int x, int y) {
+        // Logique de déplacement du pion
+        scanner = new Scanner(System.in);
+        int positionKingX = 0;
+        int positionKingY = 0;
+        int positionDeuxX = 2;
+        int positionDeuxY = 2;
+        System.out.println("Le pion est à la position : " + x + ", " + y);
+        while (!(positionDeuxX >= -1 && positionDeuxX <= 1 && positionDeuxY >= -1 && positionDeuxY <= 1 && !(positionDeuxX == 0 && positionDeuxY == 0))) {
+            System.out.println("Sur quelle case voulez-vous déplacer le pion ? la position X :");
+            positionKingX = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Sur quelle case voulez-vous déplacer le pion ? la position Y :");
+            positionKingY = scanner.nextInt();
+            scanner.nextLine();
+            positionDeuxX = positionKingX - x;
+            positionDeuxY = positionKingY - y;
+            if (!(positionDeuxX >= -1 && positionDeuxX <= 1 && positionDeuxY >= -1 && positionDeuxY <= 1 && !(positionDeuxX == 0 && positionDeuxY == 0))) {
+                System.out.println("Le pion ne peut pas se déplacer de cette façon.");
+            }
+        }
+        System.out.println("Vous avez déplacé le pion à la position : " + positionKingX + ", " + positionKingY);
+    }
 }
